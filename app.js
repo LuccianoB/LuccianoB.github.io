5d9845d163d04c043ff8a4bfd4d05ebeed12e774
LucccianoB <luccianobarberan@gmail.com>
2020-10-31 15:48:49 -0700

    Quick fixes
    Fixed links and  got rid of commented cards

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 const projects = [
     { name:"Employee Directory", link:"https://luccianob.github.io/Techdegree-Project-8/", code:"https://github.com/LuccianoB/Techdegree-Project-8"},
-    { name:"Photo Gallery", link:"https://luccianob.github.io/Techdegree-Project-5", code:"https://github.com/LuccianoB/Techdegree-Project-5"},
-    { name:"Dashboard", link:"https://luccianob.github.io/Techdegree-Project-7", code:"https://github.com/LuccianoB/Techdegree-Project-7"},
+    { name:"Photo Gallery", link:"https://luccianob.github.io/techdegree-project-5", code:"https://github.com/LuccianoB/techdegree-Project-5"},
+    { name:"Dashboard", link:"https://luccianob.github.io/Techdegree-project-7", code:"https://github.com/LuccianoB/Techdegree-project-7"},
     { name:"Wheel of Success", link:"https://luccianob.github.io/Techdegree-Project-6", code:"https://github.com/LuccianoB/Techdegree-Project-6"}
     ]
 
