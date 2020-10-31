54f22830f84423c1df542f6f9db68391faa7a347
LucccianoB <luccianobarberan@gmail.com>
2020-10-31 15:37:28 -0700

    Java Fix

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-let projects = [];
+// projects //
+
+const projects = [
+    { name:"Employee Directory", link:"https://luccianob.github.io/Techdegree-Project-8/", code:"https://github.com/LuccianoB/Techdegree-Project-8"},
+    { name:"Photo Gallery", link:"https://luccianob.github.io/Techdegree-Project-5", code:"https://github.com/LuccianoB/Techdegree-Project-5"},
+    { name:"Dashboard", link:"https://luccianob.github.io/Techdegree-Project-7", code:"https://github.com/LuccianoB/Techdegree-Project-7"},
+    { name:"Wheel of Success", link:"https://luccianob.github.io/Techdegree-Project-6", code:"https://github.com/LuccianoB/Techdegree-Project-6"}
+    ]
+
+
+// programs //
 const Container = document.querySelector(".prog-container");
+let projectHTML = '';
 
+projects.forEach((project, index) => {
+    let name = project.name;
+    let link = project.link;
+    let code = project.code;
 
-function displayEmployees(employeeData) {
-    projects = projectData;
-
-    let projectHTML = '';
-
-    projects.forEach((project, index) => {
-        let name = project.name;
-        let link = project.link;
-        let code = project.code;
-
-        projectHTML += `
-        <div class="card" data-index="${index}">
-        <img src="img/${name}.png" class="card-img-top" alt="${name}">
-        <div class="card-body text-center">
-          <h5 class="card-text">${name}</h5>
-          <div>
-            <a class="btn btn-primary" href="${link}" role="button">Live</a>
-            <a class="btn btn-primary" href="${code}" role="button">Code</a>
-          </div>
+    projectHTML += `
+    <div class="card" data-index="${index}">
+    <img src="img/${name}.png" class="card-img-top" alt="${name}">
+    <div class="card-body text-center">
+        <h5 class="card-text">${name}</h5>
+        <div>
+        <a class="btn btn-primary" href="${link}" role="button">Live</a>
+        <a class="btn btn-primary" href="${code}" role="button">Code</a>
         </div>
-      </div>
-        `
-    });
+    </div>
+    </div>
+    `
+});
 
-    Container.innerHTML = employeeHTML;
-}
+Container.innerHTML = projectHTML;
+
--- projects.js
+++
@@ -1,6 +0,0 @@
-const projects = [
-{ name:"Employee Directory", link:"https://luccianob.github.io/Techdegree-Project-8/", code:"https://github.com/LuccianoB/Techdegree-Project-8"},
-{ name:"Photo Gallery", link:"https://luccianob.github.io/Techdegree-Project-5", code:"https://github.com/LuccianoB/Techdegree-Project-5"},
-{ name:"Dashboard", link:"https://luccianob.github.io/Techdegree-Project-7", code:"https://github.com/LuccianoB/Techdegree-Project-7"},
-{ name:"Wheel of Success", link:"https://luccianob.github.io/Techdegree-Project-6", code:"https://github.com/LuccianoB/Techdegree-Project-6"}
-]
