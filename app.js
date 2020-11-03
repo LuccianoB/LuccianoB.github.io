8d107215aa509383795666e1c65982bc93aecebf
LucccianoB <luccianobarberan@gmail.com>
2020-11-03 10:38:20 -0800

    Adding skills to created cards (attempt)

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // projects //
 
 const projects = [
-    { name:"Employee Directory", link:"https://luccianob.github.io/Techdegree-Project-8/", code:"https://github.com/LuccianoB/Techdegree-Project-8"},
-    { name:"Photo Gallery", link:"https://luccianob.github.io/techdegree-project-5", code:"https://github.com/LuccianoB/techdegree-Project-5"},
-    { name:"Dashboard", link:"https://luccianob.github.io/Techdegree-project-7", code:"https://github.com/LuccianoB/Techdegree-project-7"},
-    { name:"Wheel of Success", link:"https://luccianob.github.io/Techdegree-Project-6", code:"https://github.com/LuccianoB/Techdegree-Project-6"}
-    ]
+    { name:"Employee Directory", skills: ["html", "js"] , link:"https://luccianob.github.io/Techdegree-Project-8", code:"https://github.com/LuccianoB/Techdegree-Project-8"},
+    { name:"Photo Gallery", skills: ["html", "js"], link:"https://luccianob.github.io/techdegree-project-5", code:"https://github.com/LuccianoB/techdegree-Project-5"},
+    { name:"Dashboard", skills: ["html", "js", "css"] , link:"https://luccianob.github.io/Techdegree-project-7", code:"https://github.com/LuccianoB/Techdegree-project-7"},
+    { name:"Wheel of Success", skills: ["html", "js"] , link:"https://luccianob.github.io/Techdegree-Project-6", code:"https://github.com/LuccianoB/Techdegree-Project-6"},
+    { name:"Circles Demo", skills: ["html", "css", "scss"], link:"https://luccianob.github.io/Techdegree-project-4", code:"https://github.com/LuccianoB/Techdegree-project-4"},
+    { name:"Form Example", skills: ["html", "css"], link:"https://luccianob.github.io/Techdegree-Project-3", code:"https://github.com/LuccianoB/Techdegree-Project-3"}    
+]  
 
 
 // programs //
@@ -16,9 +18,12 @@ projects.forEach((project, index) => {
     let name = project.name;
     let link = project.link;
     let code = project.code;
+    let skills = project.skills;
 
+    console.log(skills);
+    
     projectHTML += `
-    <div class="card" data-index="${index}">
+    <div class="card" data-index="${index}" id="card${index}">
     <img src="img/${name}.png" class="card-img-top" alt="${name}">
     <div class="card-body text-center">
         <h5 class="card-text">${name}</h5>
@@ -29,7 +34,12 @@ projects.forEach((project, index) => {
     </div>
     </div>
     `;
+
+    Container.innerHTML = projectHTML;
+
+    skills.forEach((skill) => {
+        document.querySelector(`#card${index}`).classList.add(`${skill}`);
+    });
 });
 
-Container.innerHTML = projectHTML;
 
