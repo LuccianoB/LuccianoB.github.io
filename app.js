572aad5b9f9da59b6a75ddf1e204fc60ddcd28b9
LucccianoB <luccianobarberan@gmail.com>
2020-11-04 13:27:07 -0800

    Added Project Filter, fixed title

--- app.js
+++ app.js
@@ -19,11 +19,9 @@ projects.forEach((project, index) => {
     let link = project.link;
     let code = project.code;
     let skills = project.skills;
-
-    console.log(skills);
     
     projectHTML += `
-    <div class="card" data-index="${index}" id="card${index}">
+    <div class="card " data-index="${index}" id="card${index}">
     <img src="img/${name}.png" class="card-img-top" alt="${name}">
     <div class="card-body text-center">
         <h5 class="card-text">${name}</h5>
@@ -36,10 +34,50 @@ projects.forEach((project, index) => {
     `;
 
     Container.innerHTML = projectHTML;
+});
+
+projects.forEach((project, index) => {
+    let skills = project.skills;
 
     skills.forEach((skill) => {
         document.querySelector(`#card${index}`).classList.add(`${skill}`);
     });
 });
 
+
+//  Filter functions    //
+
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("card");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      w3RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+}
+
+function w3RemoveClass(element, name) {
+var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+    arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+}
+element.className = arr1.join(" ");
+}
 
